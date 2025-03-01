1ca07bd95c973097811e5ea33e2931031e09132a
Daniel Montes <gdanielmontesh9@gmail.com>
2025-02-28 19:27:58 -0600

    feat: :sparkles: add custom text styles

---
+++ lib/core/theme/text_styles.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  static const TextStyle title = TextStyle(fontSize: 24, fontWeight: FontWeight.bold);
+
+  static const TextStyle subtitle = TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
+
+  static const TextStyle body = TextStyle(fontSize: 16);
+
+  static const TextStyle caption = TextStyle(fontSize: 14);
+
+  static const TextStyle button = TextStyle(fontSize: 16, fontWeight: FontWeight.bold);
+
+  static const TextStyle error = TextStyle(fontSize: 14, color: Colors.red);
+
+  static const TextStyle success = TextStyle(fontSize: 14, color: Colors.green);
+
+  static const TextStyle warning = TextStyle(fontSize: 14, color: Colors.orange);
+
+  static const TextStyle info = TextStyle(fontSize: 14, color: Colors.blue);
+
+  static const TextStyle disabled = TextStyle(fontSize: 14, color: Colors.grey);
+}
