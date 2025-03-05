979d04db445869adc0cf877b5b0bdfe3606fb8bd
Daniel Montes <gdanielmontesh9@gmail.com>
2025-03-04 19:39:11 -0600

    fix: :bug: home tab index

--- lib/core/routing/home_tab.dart
+++ lib/core/routing/home_tab.dart
@@ -9,18 +9,19 @@ class HomeTab {
 }
 
 const List<HomeTab> drawerRouter = <HomeTab>[
-  HomeTab('All', Icon(Icons.widgets_outlined), Icon(Icons.widgets)),
   HomeTab(
     'Pending Tasks',
     Icon(Icons.pending_actions_outlined),
     Icon(Icons.pending_actions),
   ),
-  HomeTab('Archived', Icon(Icons.archive_outlined), Icon(Icons.archive)),
+
   HomeTab(
     'Completed Tasks',
     Icon(Icons.task_alt_outlined),
     Icon(Icons.task_alt),
   ),
+  HomeTab('Archived', Icon(Icons.archive_outlined), Icon(Icons.archive)),
+  HomeTab('All', Icon(Icons.widgets_outlined), Icon(Icons.widgets)),
 ];
 
 const List<HomeTab> navigationRouter = <HomeTab>[
