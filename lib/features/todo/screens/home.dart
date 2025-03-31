940c1941e9b8c740d496cfd1a5b3e44bec2bc740
Daniel Montes <gdanielmontesh9@gmail.com>
2025-03-31 12:52:00 -0600

    feat: :sparkles: add ui to filter data

--- lib/features/todo/screens/home.dart
+++ lib/features/todo/screens/home.dart
@@ -15,6 +15,8 @@ import 'package:flutter_todo_list_app/features/todo/widgets/task_list_not_found.
 import 'package:flutter_expandable_fab/flutter_expandable_fab.dart';
 import 'package:logger/web.dart';
 
+enum FiltersByTasks { name, date }
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -83,6 +85,8 @@ class HomePageState extends State<HomePage> {
       _fetchTasks(index);
     });
   }
+
+  FiltersByTasks? _character = FiltersByTasks.name;
 
   @override
   Widget build(BuildContext context) {
@@ -170,7 +174,96 @@ class HomePageState extends State<HomePage> {
                   ),
                   FloatingActionButton(
                     heroTag: null,
-                    onPressed: () {},
+                    onPressed: () {
+                      showModalBottomSheet(
+                        context: context,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.vertical(
+                            top: Radius.circular(20),
+                          ),
+                        ),
+                        builder:
+                            (context) => StatefulBuilder(
+                              builder:
+                                  (
+                                    BuildContext context,
+                                    StateSetter setModalState,
+                                  ) => Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(16.0),
+                                      child: Column(
+                                        children: <Widget>[
+                                          Padding(
+                                            padding: const EdgeInsets.only(
+                                              left: 24,
+                                              right: 24,
+                                              top: 24,
+                                              bottom: 12,
+                                            ),
+                                            child: Align(
+                                              alignment: Alignment.centerLeft,
+                                              child: Text(
+                                                'Filter data by:',
+                                                style: TextStyle(
+                                                  fontSize: 22,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                          Divider(
+                                            indent: 24,
+                                            endIndent: 24,
+                                            color: AppColors.disabled,
+                                          ),
+                                          ListTile(
+                                            title: const Text(
+                                              'Name',
+                                              style: TextStyle(fontSize: 16),
+                                            ),
+                                            leading: Radio<FiltersByTasks>(
+                                              value: FiltersByTasks.name,
+                                              groupValue: _character,
+                                              onChanged: (value) {
+                                                print(value);
+                                                // _character = value;
+                                                setModalState(() {
+                                                  _character = value!;
+                                                });
+                                                setState(() {
+                                                  _character = value!;
+                                                });
+                                              },
+                                            ),
+                                          ),
+                                          ListTile(
+                                            title: const Text(
+                                              'Date',
+                                              style: TextStyle(fontSize: 16),
+                                            ),
+                                            leading: Radio<FiltersByTasks>(
+                                              value: FiltersByTasks.date,
+                                              groupValue: _character,
+                                              onChanged: (value) {
+                                                print(value);
+                                                // _character = value;
+                                                setModalState(() {
+                                                  _character = value!;
+                                                });
+                                                setState(() {
+                                                  _character = value!;
+                                                });
+                                              },
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                            ),
+                      );
+                    },
+
                     shape: const CircleBorder(),
                     child: const Icon(Icons.filter_alt),
                   ),
